92e76f2a5d4357cb7c2315140ca3343e667cd090
Théo Percheron <77283423+Tpercher@users.noreply.github.com>
2021-02-23 16:29:55 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<section id="competences" >
+			<h3> Points forts </h3>
+					<article>
+						<h5>HTML5 & CSS3</h5>
+						<div class="conteneur_progress_bar">
+							<div class="commun_progress_style" id="progress_bar_html"><span class="pourcentage">85%</span></div>
+						</div>
+
+						<h5>Photoshop</h5>
+						<div class="conteneur_progress_bar">
+							<div class="commun_progress_style" id="progress_bar_ps"><span class="pourcentage">65%</span></div>
+						</div>
+
+						<h5>Premiere Pro</h5>
+						<div class="conteneur_progress_bar">
+							<div class="commun_progress_style" id="progress_bar_pp"><span class="pourcentage">70%</span></div>
+						</div>
+
+						<h5>Bootstrap</h5>
+						<div class="conteneur_progress_bar">
+							<div class="commun_progress_style" id="progress_bar_bootstrap"><span class="pourcentage">80%</span></div>
+						</div>
+
+						<h5>Wordpress</h5>
+						<div class="conteneur_progress_bar">
+							<div class="commun_progress_style" id="progress_bar_cms"><span class="pourcentage">80%</span></div>
+						</div>
+					</article>
+		</section>
